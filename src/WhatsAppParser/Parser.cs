0ae866cacbb3d1d082bc3f3d5138b25cbba94eaa
fatderda <fatderda@hotmail.com>
2024-11-26 00:28:59 +0100

    fix english parsing

--- src/WhatsAppParser/Parser.cs
+++ src/WhatsAppParser/Parser.cs
@@ -6,20 +6,17 @@ using System.Text.RegularExpressions;
 
 namespace WhatsAppParser;
 
-public class Parser
-{
+public class Parser {
     private readonly string _filePath;
     private Regex _messageRegex;
     private string _dateFormat;
 
-    public Parser(string filePath, WhatsAppLanguage language)
-    {
+    public Parser(string filePath, WhatsAppLanguage language) {
         _filePath = filePath ?? throw new ArgumentNullException(nameof(filePath));
         ConfigureLanguage(language);
     }
 
-    public IEnumerable<Message> Messages()
-    {
+    public IEnumerable<Message> Messages() {
         using var reader = File.OpenText(_filePath);
         MessageBuilder messageBuilder = null;
 
@@ -39,11 +36,12 @@ public class Parser
                 }
 
                 // Prepare for next message
-                var timestamp =
-                    DateTime.ParseExact(match.Groups[1].Value, _dateFormat, CultureInfo.InvariantCulture);
+                var date = match.Groups[1].Value?.Replace('\u202F', ' '); //normalize non-breaking whitespace
+                var timestamp = DateTime.ParseExact(date, _dateFormat, CultureInfo.InvariantCulture);
                 messageBuilder = new MessageBuilder(timestamp, match.Groups[2].Value);
                 messageBuilder.AppendContentLine(match.Groups[4].Value);
-            } else
+            }
+            else
                 // Line appends to the existing message
                 // There might be the case that this line is just invalid and there is no builder from previous lines
                 messageBuilder?.AppendContentLine(line);
@@ -52,12 +50,11 @@ public class Parser
         if (messageBuilder != null) yield return messageBuilder.Build();
     }
 
-    private void ConfigureLanguage(WhatsAppLanguage language)
-    {
+    private void ConfigureLanguage(WhatsAppLanguage language) {
         switch (language) {
             case WhatsAppLanguage.English:
-                _messageRegex = new Regex(@"^(\d{2}/\d{2}/\d{4}, \d{2}:\d{2}) - ([^:]+)(: )?(.*)?$", RegexOptions.CultureInvariant | RegexOptions.Compiled);
-                _dateFormat = "dd/MM/yyyy, HH:mm";
+                _messageRegex = new Regex(@"^(\d{1,2}\/\d{1,2}\/\d{2}, \d{1,2}:\d{2}\s[AP]M) - ([^:]+)(: )?(.*)?$", RegexOptions.CultureInvariant | RegexOptions.Compiled);
+                _dateFormat = "M/d/yy, h:mm tt";
                 break;
             case WhatsAppLanguage.German:
                 _messageRegex = new Regex(@"^(\d{2}\.\d{2}\.\d{2}, \d{2}:\d{2}) - ([^:]+)(: )?(.*)?$", RegexOptions.CultureInvariant | RegexOptions.Compiled);
